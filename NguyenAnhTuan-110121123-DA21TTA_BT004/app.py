cae3f4c1f164228ae86c2b5a5027231b520962ea
ngg_anhtuan <ng_anhtuan@outlook.com>
2025-05-04 18:41:45 +0700

    Auto commit

--- NguyenAnhTuan-110121123-DA21TTA_BT004/app.py
+++ NguyenAnhTuan-110121123-DA21TTA_BT004/app.py
@@ -139,7 +139,7 @@ def delete_image(index):
     session['uploaded_urls'] = uploaded_urls
     return redirect(url_for("index"))
 
-# import os
-# port = int(os.environ.get("PORT", 5000))
-# app.run(host="0.0.0.0", port=port)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
